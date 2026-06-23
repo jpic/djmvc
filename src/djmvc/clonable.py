0b036ce5b4fe15234fbf8bfe62401fa60e714026
jpic <jpic@yourlabs.org>
2026-06-23 16:16:10 +0200

    clonablewip

--- src/djmvc/clonable.py
+++ src/djmvc/clonable.py
@@ -8,7 +8,4 @@ class Clonable:
         If a model is found, it will prefix the class name with the model.
         """
         name = cls.__name__
-        model = attributes.get('model', getattr(cls, 'model', None))
-        if model and model.__name__ not in cls.__name__:
-            name = '{}{}'.format(model.__name__, cls.__name__)
         return type(name, (cls,) + mixins, attributes)
