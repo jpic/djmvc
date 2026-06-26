3c298ef9a4b5fbccce00a0c32a8c35b94af9debf
jpic <jpic@yourlabs.org>
2026-06-26 06:50:59 +0200

    Implement ModelFormMixin, fixes create/update view

--- src/djmvc/views/create.py
+++ src/djmvc/views/create.py
@@ -1,12 +1,11 @@
 from django.views import generic
 
 from ..view import ViewMixin
-from ..model import ModelMixin
-from .form import FormMixin
+from .modelform import ModelFormMixin
 from .template import TemplateMixin
 
 
-class CreateView(ModelMixin, TemplateMixin, FormMixin, ViewMixin, generic.CreateView):
+class CreateView(ModelFormMixin, TemplateMixin, ViewMixin, generic.CreateView):
     tags = ['model']
     default_template_name = 'djmvc/form.html'
     icon = 'plus-circle'
--- src/djmvc/views/modelform.py
+++ src/djmvc/views/modelform.py
@@ -1,6 +1,13 @@
+from django.forms import modelform_factory
+
 from ..model import ModelMixin
 from .form import FormMixin
 
 
 class ModelFormMixin(ModelMixin, FormMixin):
-    pass
+    fields = '__all__'
+
+    def get_form_class(self):
+        if getattr(self, 'form_class', None):
+            return self.form_class
+        return modelform_factory(self.model, fields=self.fields)
